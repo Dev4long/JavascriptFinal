54a93ddf8c5ab07d97e85439cd8f56c70614acc1
denis kadrich <dev4long@live.com>
2023-07-09 23:37:13 -0400

    almost done with slides

--- final/src/HomePage.js
+++ final/src/HomePage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Slides from './Slides';
+import "./HomePage.css"
 
 const HomePage = ({ goBack }) => {
   return (
-    <div>
+    <div className='container'>
+      <div className = "homePage">
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
-      <Button variant="contained" color="secondary" onClick={goBack}>
+      <Button variant="contained" color="secondary"  type="submit" onClick={goBack}>
         Go Back
       </Button>
+      </div>
+    <div className="homePageContainer">
+      <Slides />
+    </div>
     </div>
   );
 };
